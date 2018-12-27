97343ca0888e3d799440d790a8570f5c924439aa
brooke ryan <brooke.ryan@modaoperandi.com>
2018-12-27 18:15:02 -0500

    [routing] npm run start

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,28 +1,24 @@
 import { Action } from '@ngrx/store';
-import {User} from '../model/user.model';
+import { User } from '../model/user.model';
 
-
-//
+// contains one entry per action that belongs to module
 export enum AuthActionTypes {
   LoginAction = '[Login] Action',
   LogoutAction = '[Logout] Action',
 }
 
-
+// login action
 export class Login implements Action {
-
+// unique string identifier from above
   readonly type = AuthActionTypes.LoginAction;
 
-  constructor(public payload: {user: User}) {
+  constructor(public payload: { user: User }) {}
 
-  }
 }
 
 
 export class Logout implements Action {
-
   readonly type = AuthActionTypes.LogoutAction;
-
 
 }
 
